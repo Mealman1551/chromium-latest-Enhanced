daa20a81712b5f68f09332193d98103be96f4b72
Mealman1551 <nathandubuy4@gmail.com>
2025-10-23 15:48:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,35 +4,45 @@ set -e
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 INSTALL_DIR="/opt/chromium-latest"
 DESKTOP_FILE="/usr/share/applications/chromium-latest.desktop"
+ICON_PATH="/usr/share/icons/hicolor/scalable/apps/chromium-latest.svg"
 
 echo "Updating Chromium..."
 LASTCHANGE_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2FLAST_CHANGE?alt=media"
 REVISION=$(curl -s -S $LASTCHANGE_URL)
 echo "Latest revision is $REVISION"
 
-# Maak een tijdelijke map voor het downloaden
+# Maak tijdelijke map
 TMP_DIR=$(mktemp -d)
 
+# Download Chromium zip
 ZIP_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F$REVISION%2Fchrome-linux.zip?alt=media"
 ZIP_FILE="$TMP_DIR/chrome-linux.zip"
-
 echo "Fetching $ZIP_URL"
 curl -# -o "$ZIP_FILE" "$ZIP_URL"
 
+# Unzip
 echo "Unzipping..."
 unzip -q "$ZIP_FILE" -d "$TMP_DIR"
 
+# Update symlink
 rm -f "$SCRIPT_DIR/latest"
 ln -s "$TMP_DIR/chrome-linux" "$SCRIPT_DIR/latest"
 
+# Installeren
 echo "Installing to $INSTALL_DIR..."
 sudo rm -rf "$INSTALL_DIR"
 sudo mkdir -p "$INSTALL_DIR"
 sudo cp -r "$SCRIPT_DIR/latest/." "$INSTALL_DIR/"
 
+# Symlink naar /usr/bin
 echo "Creating symlink /usr/bin/chromium..."
 sudo ln -sf "$INSTALL_DIR/chrome" /usr/bin/chromium
 
+# Download SVG icon
+echo "Downloading SVG icon..."
+sudo curl -L -o "$ICON_PATH" "https://upload.wikimedia.org/wikipedia/commons/2/28/Chromium_Logo.svg"
+
+# .desktop bestand maken
 echo "Creating .desktop file..."
 sudo tee "$DESKTOP_FILE" > /dev/null <<EOL
 [Desktop Entry]
@@ -40,13 +50,13 @@ Version=1.0
 Name=Chromium Latest
 Comment=The latest Chromium Browser
 Exec=$INSTALL_DIR/chrome %U
-Icon=$INSTALL_DIR/product_logo_48.png
+Icon=$ICON_PATH
 Terminal=false
 Type=Application
 Categories=Network;WebBrowser;
 EOL
 
-# Verwijder tijdelijke map
+# Opruimen tijdelijke map
 rm -rf "$TMP_DIR"
 
 echo "Installation completed."
